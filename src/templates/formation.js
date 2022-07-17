194a8468f7ef1244cb3dc5dd019a009c85980249
anthow <anthonyenglebert@gmail.com>
2022-07-17 18:21:00 +0200

    Update formation.js

--- src/templates/formation.js
+++ src/templates/formation.js
@@ -1,11 +1,36 @@
 import * as React from "react"
 import { graphql } from "gatsby"
 import Layout from "../components/layout"
+import { GatsbyImage } from "gatsby-plugin-image"
 
 const formation = ({ data }) => (
   <>
     <Layout>
-      {data.datoCmsFormation.introduction}
+    <article class=" w-12/12 p-4 md:p-0  flex flex-col md:grid grid-cols-2 pb-20 bg-primary-color ">
+
+    <section class="flex m-2 md:py-0   flex-col   h-min     p-4  
+    justify-center md:items-start  order-2 md:order-1"
+          >
+            <h2 class=" w-12/12 m-auto text-center text-white pt-2 font-bold title text-3xl mb-8">
+              {data.datoCmsFormation.titreFormation}
+            </h2>
+            <div
+              dangerouslySetInnerHTML={{
+                __html: data.datoCmsFormation.introduction,
+              }}
+            />
+                      </section>
+
+          <section class="   md:p-20  bg-primary-color col-span-2  md:mr-10 order-1 md:order-2">
+            <GatsbyImage
+              image={data.datoCmsFormation.imageFormation.gatsbyImageData}
+              className="w-full"
+              alt={data.datoCmsFormation.imageFormation.gatsbyImageData}
+              loading="lazy"
+            />
+          
+          </section>
+      </article>
     </Layout>
   </>
 )
